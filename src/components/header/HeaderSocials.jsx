b282062278ec55b9ca049a710a37add42df98178
Eggsdoo <usama.uyk@hotmail.co.uk>
2023-10-04 11:51:53 -0400

    header social changes

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -6,7 +6,7 @@ import {FaGithubSquare} from 'react-icons/fa'
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-        <a href="https://www.linkedin.com/in/usamaykhan/" rel="noreferrer" target='_blank'><FaLinkedin/></a>
+        <a href="https://www.linkedin.com/in/uykhan/" rel="noreferrer" target='_blank'><FaLinkedin/></a>
         <a href="https://github.com/Eggsdoo" rel="noreferrer" target='_blank'><FaGithubSquare/></a>
         {/* <a href="https://dribbble.com" rel="noreferrer" target='_blank'><FaDribbbleSquare/></a> */}
     </div>
